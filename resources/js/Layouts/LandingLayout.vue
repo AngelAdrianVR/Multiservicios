b394d8af612b54cea30d621a4d105fc1d360efc8
Angel Vazquez <angelvazquez470@gmail.com>
2024-09-17 13:02:19 -0600

    guardando cambios de pagina web. configuracion de estilos, applayout modifcado, navegación modificada

---
+++ resources/js/Layouts/LandingLayout.vue
@@ -0,0 +1,149 @@
+<script setup>
+import { ref } from 'vue';
+import { Head, Link, router } from '@inertiajs/vue3';
+import ApplicationMark from '@/Components/ApplicationMark.vue';
+import Banner from '@/Components/Banner.vue';
+import Dropdown from '@/Components/Dropdown.vue';
+import DropdownLink from '@/Components/DropdownLink.vue';
+import NavLink from '@/Components/NavLink.vue';
+import ResponsiveNavLink from '@/Components/ResponsiveNavLink.vue';
+
+defineProps({
+    title: String,
+});
+
+const showingNavigationDropdown = ref(false);
+
+const switchToTeam = (team) => {
+    router.put(route('current-team.update'), {
+        team_id: team.id,
+    }, {
+        preserveState: false,
+    });
+};
+
+const logout = () => {
+    router.post(route('logout'));
+};
+
+const goToWhatsApp = () => {
+  const url = "https://api.whatsapp.com/send?phone=523313825055&text=Hola!%20vi%20tu%20página%20de%20fletes%20y%20reparaciones.%20Me%20interesa%20su%20servicio!";
+  window.open(url, "_blank", "noopener,noreferrer");
+};
+</script>
+
+<style>
+/* Estilos para la barra de navegación */
+/* .navbar {
+    position: absolute;
+    top: 0;
+    background-color: #ffffff;
+    opacity: 0.9;
+}
+.fixed-navbar {
+    position: fixed;
+    box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.1);
+    z-index: 100;
+}
+html {
+    scroll-behavior: smooth;
+}
+.custom-style .el-segmented {
+  --el-segmented-bg-color: #818181;
+  --el-segmented-color: #000;
+  --el-segmented-item-selected-color: #FFFFFF;
+  --el-segmented-item-selected-bg-color: #a1a1a1;
+  --el-border-radius-base: 16px;
+} */
+</style>
+
+<template>
+    <div>
+        <Head :title="title" />
+
+        <Banner />
+
+        <div class="min-h-screen bg-[#343434]">
+            <nav :class="['navbar', { 'fixed-navbar': isNavbarFixed }]" class="bg-[#343434] py-1 lg:mx-16">
+                <!-- Primary Navigation Menu -->
+                <div class="max-w-full px-4 sm:px-6 lg:px-8">
+                    <div class="flex justify-between h-16">
+                        <div class="flex justify-between w-full mr-16">
+                            <!-- Logo -->
+                            <div class="shrink-0 flex items-center">
+                                <Link :href="route('home')">
+                                    <ApplicationMark class="block h-12 w-auto" />
+                                </Link>
+                            </div>
+
+                            <!-- Navigation Links -->
+                            <div class="hidden space-x-10 sm:-my-px sm:ms-10 sm:flex">
+                                <NavLink :href="route('home')" :active="route().current('home')">
+                                    Inicio
+                                </NavLink>
+                                <NavLink :href="route('dashboard')" :active="route().current('dashboard')">
+                                    Servicios
+                                </NavLink>
+                                <button @click="goToWhatsApp" class="text-gray-400 hover:text-white">
+                                    <i class="fa-brands fa-whatsapp"></i>
+                                </button>
+
+                            </div>
+                        </div>
+
+                        <!-- Hamburger -->
+                        <div class="-me-2 flex items-center sm:hidden">
+                            <button class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out" @click="showingNavigationDropdown = ! showingNavigationDropdown">
+                                <svg
+                                    class="h-6 w-6"
+                                    stroke="currentColor"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                >
+                                    <path
+                                        :class="{'hidden': showingNavigationDropdown, 'inline-flex': ! showingNavigationDropdown }"
+                                        stroke-linecap="round"
+                                        stroke-linejoin="round"
+                                        stroke-width="2"
+                                        d="M4 6h16M4 12h16M4 18h16"
+                                    />
+                                    <path
+                                        :class="{'hidden': ! showingNavigationDropdown, 'inline-flex': showingNavigationDropdown }"
+                                        stroke-linecap="round"
+                                        stroke-linejoin="round"
+                                        stroke-width="2"
+                                        d="M6 18L18 6M6 6l12 12"
+                                    />
+                                </svg>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Responsive Navigation Menu -->
+                <div :class="{'block': showingNavigationDropdown, 'hidden': ! showingNavigationDropdown}" class="sm:hidden">
+                    <div class="pt-2 pb-3 space-y-1">
+                        <ResponsiveNavLink :href="route('home')" :active="route().current('home')">
+                            Inicio
+                        </ResponsiveNavLink>
+                        <ResponsiveNavLink :href="route('dashboard')" :active="route().current('dashboard')">
+                            Servicios
+                        </ResponsiveNavLink>
+                    </div>
+                </div>
+            </nav>
+
+            <!-- Page Heading -->
+            <header v-if="$slots.header" class="bg-white shadow">
+                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                    <slot name="header" />
+                </div>
+            </header>
+
+            <!-- Page Content -->
+            <main>
+                <slot />
+            </main>
+        </div>
+    </div>
+</template>
---
+++ resources/js/Pages/Landing/Inicio.vue
@@ -0,0 +1,189 @@
+<template>
+    <LandingLayout :title="'Bienvenidos'">
+        <main class="">
+            <!-- Seccion 1 -->
+            <section class="lg:grid grid-cols-2 gap-12 items-center py-7 px-5 lg:py-14 lg:px-20">
+                <div class="my-9 text-center lg:text-left lg:ml-14">
+                    <h2 class="text-xl md:text-3xl text-white font-bold mb-16">Mantenimiento experto y mudanza sin complicaciones, todo en un solo lugar</h2>
+                    <PrimaryButton @click="goToWhatsApp">Contáctanos</PrimaryButton>
+                </div>
+                
+                <el-carousel trigger="click" :height="carouselHeight">
+                    <el-carousel-item v-for="item in groupImages1" :key="item">
+                        <img :src="item" alt="">
+                    </el-carousel-item>
+                </el-carousel>
+            </section>
+
+            <!-- Seccion 2 -->
+            <section>
+                <h2 class="text-2xl font-bold text-white text-center mt-36 mb-5">NUESTROS SERVICIOS</h2>
+                <article class="bg-[#292929] py-7 px-14 flex space-x-5 relative">
+                    <div class="w-[40%]">
+                        <h3 class="text-xl text-white font-bold mb-10 mt-7">FLETES Y MUDANZAS</h3>
+                        <p class="text-white text-lg">Ya sea que necesites trasladar muebles dentro de la ciudad o mudarte a otra región, nuestro equipo maneja cada detalle con precisión.</p>
+                    </div>
+
+                    <!-- carousel -->
+                    <Carousel class="w-[60%]" v-bind="popularSettings" :breakpoints="breakpoints">
+                        <Slide v-for="slide in groupImages2" :key="slide">
+                            <img :src="slide" alt="">
+                        </Slide>
+
+                        <template #addons>
+                            <Navigation />
+                        </template>
+                    </Carousel>
+
+                    <!-- footer -->
+                    <div class="absolute bottom-2 left-2">
+                        <svg width="106" height="36" viewBox="0 0 106 36" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M66.9838 5.8904C76.3902 7.81014 81.4576 9.75287 89.6001 16.8356H77.4759V15.4383H85.1701C79.1546 9.91102 74.8704 8.32317 66.9838 5.8904ZM85.1701 18.2328C87.8718 18.0313 89.1867 18.1651 90.9991 18.9314L91.4654 21.726L104.989 25.2191V31.2739C105.108 27.4215 104.341 25.9099 100.325 25.2191L85.1701 22.1917V18.2328ZM82.8385 34.9999C90.7707 24.9756 94.4407 35.0381 94.4964 34.9999C94.5522 34.9617 92.114 22.9843 82.8385 34.9999ZM73.279 16.8356H74.4448V29.178H73.279V16.8356ZM63.4864 29.6437V1L1 1.69863L61.6211 2.63013V29.6437H63.4864ZM60.2222 7.05477V9.38353L1 8.45203L60.2222 7.05477ZM60.2222 16.8356V18.9314L1 18.2328L60.2222 16.8356ZM60.2222 26.3835V28.4794L1.23316 28.0136L60.2222 26.3835ZM15.9221 34.0684C24.1022 22.8017 27.462 35.6768 26.4142 34.0684C25.3665 32.46 23.0259 26.1058 15.9221 34.0684ZM1.46632 33.8355C7.42052 28.5991 9.96917 28.468 12.4248 34.0684C10.0001 26.4954 7.25771 27.8582 1.46632 33.8355Z" stroke="#9B9B9B" stroke-width="1.5"/>
+                        </svg>
+                        <p class="text-white italic">Nos aseguramos que cada objeto llegue a su destino de forma segura y a tiempo.</p>
+                    </div>
+                </article>
+            </section>
+        </main>
+    </LandingLayout>
+</template>
+
+<script>
+//general
+import LandingLayout from '@/Layouts/LandingLayout.vue';
+import PrimaryButton from '@/Components/PrimaryButton.vue';
+
+//imagenes
+import Group1_1 from '@/../../public/images/Group1_1.png';
+import Group1_2 from '@/../../public/images/Group1_2.png';
+import Group1_3 from '@/../../public/images/Group1_3.png';
+import Group1_4 from '@/../../public/images/Group1_4.png';
+import Group2_1 from '@/../../public/images/Group2_1.png';
+import Group2_2 from '@/../../public/images/Group2_2.png';
+import Group2_3 from '@/../../public/images/Group2_3.png';
+import Group2_4 from '@/../../public/images/Group2_4.png';
+
+//carousel
+import { Carousel, Navigation, Pagination, Slide } from 'vue3-carousel';
+import 'vue3-carousel/dist/carousel.css';
+
+export default {
+data() {
+    
+    return {
+        carouselHeight: '500px', //tamaño del carusel de elemen plus
+        groupImages1: [
+            Group1_1,
+            Group1_2,
+            Group1_3,
+            Group1_4
+        ],
+        groupImages2: [
+            Group2_1,
+            Group2_2,
+            Group2_3,
+            Group2_4
+        ],
+        popularSettings: {
+            wrapAround: true,
+            itemsToShow: 2,
+            snapAlign: 'start',
+            autoplay: 4000,
+        },
+        breakpoints: {
+            // 700px and up
+            700: {
+                itemsToShow: 2,
+                snapAlign: 'start',
+            },
+            // 1024 and up
+            1024: {
+                itemsToShow: 2,
+                snapAlign: 'start',
+            },
+        },
+    }
+},
+components:{
+    LandingLayout,
+    PrimaryButton,
+    Navigation,
+    Pagination,
+    Carousel,
+    Slide,
+},
+props:{
+
+},
+methods:{
+    goToWhatsApp() {
+      const url = "https://api.whatsapp.com/send?phone=523314179923&text=Hola!%20vi%20tu%20página%20de%20fletes%20y%20reparaciones.%20Me%20interesa%20su%20servicio!";
+      window.open(url, "_blank", "noopener,noreferrer");
+    },
+    updateCarouselHeight() {
+      const width = window.innerWidth;
+      if (width < 430) {
+        this.carouselHeight = '300px';
+      } else if (width >= 430 && width < 1024) {
+        this.carouselHeight = '480px';
+      } else if (width >= 1024 && width < 1350) {
+        this.carouselHeight = '330px';
+      } else {
+        this.carouselHeight = '500px';
+      }
+    },
+},
+mounted() {
+  this.updateCarouselHeight();
+  window.addEventListener('resize', this.updateCarouselHeight);
+},
+beforeUnmount() {
+  window.removeEventListener('resize', this.updateCarouselHeight);
+},
+}
+</script>
+
+<style>
+.el-carousel__item h3 {
+  color: #475669;
+  opacity: 0.75;
+  line-height: 150px;
+  margin: 0;
+  text-align: center;
+}
+
+.el-carousel__item:nth-child(2n) {
+  background-color: transparent;
+}
+
+.el-carousel__item:nth-child(2n + 1) {
+  background-color: transparent;
+}
+
+.el-carousel__arrow {
+    background-color: rgba(99, 98, 98, 0.562);
+    /* Fondo semitransparente */
+    border-radius: 50%;
+    /* Bordes redondeados */
+    color: white;
+    /* Color del icono */
+}
+
+.el-carousel__arrow--left {
+    left: 10px;
+    /* Ajusta la posición de la flecha izquierda */
+}
+
+.el-carousel__arrow--right {
+    right: 10px;
+    /* Ajusta la posición de la flecha derecha */
+}
+
+/* Puedes agregar más estilos personalizados aquí */
+.el-carousel__arrow:hover {
+    background-color: rgba(131, 130, 130, 0.562);
+    /* Cambia el fondo al hacer hover */
+    /* transform: scale(1.1); Efecto de escalado al hacer hover */
+}
+
+</style>
